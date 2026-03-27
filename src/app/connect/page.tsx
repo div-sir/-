978a17ee545c80f289b3d68a12d3ca638f584172
Solilium <steven33152239@gmail.com>
2026-03-27 13:42:38 -0700

    更新網站內容

---
+++ src/app/connect/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{ padding: 32, maxWidth: 720 }}>
+      <h1>{"新頁面"}</h1>
+      <p>此頁由 SiteMaster 建立，可在 App 右側「網頁元素」調整標題與段落。</p>
+    </main>
+  );
+}
